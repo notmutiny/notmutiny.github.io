c074cc586a7aacb1a9ec9920a8f76a8f1612a74d
mutiny <notmutiny@users.noreply.github.com>
2018-08-05 19:56:50 -0700

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+    
+    // update external css theme on page load
+    var theme = window.localStorage.getItem('theme');
+    if (theme) {
+        $('#theme').attr('href', theme);
+        if (theme == 'styles/light.css') // change fontawesome moon icon to sun
+            $('#theme-icon').removeClass('fas fa-moon').addClass('fas fa-sun');
+    }
+});
+
+function toggleSlider() {
+    var test = document.getElementById("slider").style.display;
+
+    if (test == "none") {
+        document.getElementById("slider").style.display = "block";
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#slider").offset().top
+        }, 500);
+    } else
+        document.getElementById("slider").style.display = "none";
+}
+// -- convenience functions -- //
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+
+// -- logo functions -- //
+
+var logoCache = "";
+
+function randomLogo(element) { // change superintendents expression onmouseover
+    var logoFaces = ["angry", "closed", "confused", "happy", "tired", "worried"];
+
+    if (logoCache) // stop logo from showing a face twice
+        logoFaces.splice(logoFaces.indexOf(logoCache), 1);
+
+    logoCache = logoFaces[getRandomInt(0, logoFaces.length)];
+    element.src = "logo/eyes/" + logoCache + ".png";
+}
+
+function restoreLogo(element) {
+    element.src = "logo/eyes/default.png"
+}
+
+
+// -- footer functions -- //
+
+function toggleTheme() {
+    if ($('#theme').attr('href') == 'styles/dark.css') {
+        $('#theme').attr('href', 'styles/light.css');
+        $('#theme-icon').removeClass('fas fa-moon').addClass('fas fa-sun');
+        window.localStorage.setItem('theme', 'styles/light.css');
+    } else {
+        $('#theme').attr('href', 'styles/dark.css');
+        $('#theme-icon').removeClass('fas fa-sun').addClass('fas fa-moon');
+        window.localStorage.setItem('theme', 'styles/dark.css');
+    }
+}
